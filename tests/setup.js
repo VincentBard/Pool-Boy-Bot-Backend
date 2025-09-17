280d935a0353bf981d727f0f8caaee81b4d22c01
VincentBard <113477761+VincentBard@users.noreply.github.com>
2025-09-17 19:58:19 -0400

    update

--- tests/setup.js
+++ tests/setup.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { jest } from "@jest/globals";  // ✅ add this
 
 dotenv.config();
+
 
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+    
   });
 });
+jest.setTimeout(30000); // 30 seconds
 
 afterAll(async () => {
   await mongoose.connection.dropDatabase();
