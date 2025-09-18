08a7305b594ddad39b26bd00c96f8637e55e50a4
VincentBard <113477761+VincentBard@users.noreply.github.com>
2025-09-18 08:05:49 -0400

    Update readings.js

--- routes/readings.js
+++ routes/readings.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Reading from "../database/readingSchema.js";
+import Reading from "../database/sensorReadingSchema.js";
 import Device from "../database/deviceSchema.js";
 import authMiddleware from "../middleware/auth.js";
 import ensureUser from "../middleware/ensureUser.js";
