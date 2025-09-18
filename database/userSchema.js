847b094cb8f3dd4122808a4d8be1c91720273688
VincentBard <113477761+VincentBard@users.noreply.github.com>
2025-09-17 20:58:20 -0400

    Update userSchema.js

--- database/userSchema.js
+++ database/userSchema.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   passwordHash: { type: String, required: true },
   fullName: { type: String },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  passwordHash: { type: String }, // ✅ make it optional
 });
 
 export default mongoose.model('User', UserSchema);
