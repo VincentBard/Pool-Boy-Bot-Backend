c9d434a90ce80f6b6cdfe6d591005ef1fb8e9f08
VincentBard <113477761+VincentBard@users.noreply.github.com>
2025-11-15 15:06:52 -0500

    Update alertSchema.js

--- database/alertSchema.js
+++ database/alertSchema.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const AlertSchema = new mongoose.Schema({
-  deviceId: { type: mongoose.Schema.Types.ObjectId, ref: "Device", required: true },
+  device: { type: mongoose.Schema.Types.ObjectId, ref: "Device", required: true },
   alertType: { type: String }, // e.g. "High Temp", "Low Chlorine"
   message: { type: String, required: true },
   severity: { type: String, enum: ["info", "warning", "critical"], default: "info" },
